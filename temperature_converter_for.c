71bd0ff95f9d44c78ac0f832aace1a28414f22f4
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-04-04 11:13:55 +0300

    fixed build

--- temperature_converter_for.c
+++ temperature_converter_for.c
@@ -1,14 +1,14 @@
 #include "stdio.h"
 
-#define LOWER = 0
-#define UPPER = 300
-#define STEP = 20
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
 
 float convertFahrenheitToCelsius(int);
 
 int main() {
     for (int fahr = LOWER; fahr <= UPPER; fahr += STEP) {
-        printf("%3d\t\t%6.1f\n", fahr, convertFahrenheitToCelsiu(fahr));
+        printf("%3d\t\t%6.1f\n", fahr, convertFahrenheitToCelsius(fahr));
     }
     
     return 0;
