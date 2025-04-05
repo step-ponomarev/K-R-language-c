29587226349f5724a7ba9cc23dbfe98d901af3c1
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-04-05 16:28:45 +0300

    wc implemeted

---
+++ wc.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+
+#define IN  1
+#define OUT 0
+
+char isSpace(int ch);
+
+int main() {
+    int lc = 0;
+    int wc = 0;
+    int sc = 0;
+    char state = OUT;
+
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        sc++;
+
+        if (ch == '\n') {
+            lc++;
+        } else if (isSpace(ch)) {
+            state = OUT;
+        } else {
+            if (state == OUT) {
+                state = IN;
+                wc++;
+            }
+        }
+    }
+
+    printf("Line Count: %3d | Word Count: %3d | Symbol Count: %3d", lc, wc, sc);
+
+    return 0;
+}
+
+char isSpace(int ch) {
+    return ch == ' ' || ch == '\t';
+}
