b8f577cda2b81ab69c06b749ba9c98c3344c7b04
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-04-11 09:31:19 +0300

    added comments remover

---
+++ comment_remover.c
@@ -0,0 +1,115 @@
+#include "stdio.h"
+#include "stringutils.h"
+#include "stdlib.h"
+
+#define START_COMMENT '/'
+#define START_MULTILINE_COMMENT '*'
+#define START_STRING '\"'
+#define START_SYMBOL '\''
+
+void handleMultilineComment();
+void handleSingleLineComment();
+int handleLiteralSymbol(char* res, int curr);
+int handleStringLiteral(char* res, int curr);
+
+int main() {
+    char* res = malloc(sizeof(char) * (80 * 1000));
+    int curr = 0;
+
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        if (ch == START_COMMENT) {
+            int tmp = ch;
+            if ((ch = getchar()) == EOF) {
+                break;
+            }
+
+            if (ch == START_MULTILINE_COMMENT) {
+                handleMultilineComment();
+                continue;
+            }
+
+            if (ch == START_COMMENT) {
+                handleSingleLineComment();
+                res[curr++] = '\n';
+                continue;
+            }
+            
+            res[curr++] = tmp;
+            res[curr++] = ch;
+            continue;
+        }
+
+        if (ch == START_STRING) {
+            curr = handleStringLiteral(res, curr);
+            continue;
+        }
+
+        if (ch == START_SYMBOL) {
+            curr = handleLiteralSymbol(res, curr);
+            continue;
+        }
+
+        res[curr++] = ch;
+    }
+
+    res[curr++] = '\0';
+    printf("\n%s", res);
+
+    return 0;
+}
+
+
+void handleSingleLineComment() {
+    char* line = NULL;
+    readline(&line);
+    free(line);
+}
+
+void handleMultilineComment() {
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        if (ch != START_MULTILINE_COMMENT) {
+            continue;
+        }
+
+        ch = getchar();
+        if (ch == EOF || ch == START_COMMENT) {
+            break;
+        }
+    }
+}
+
+int handleLiteralSymbol(char* res, int curr) {
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        res[curr++] = ch;
+        if (ch == '\\') {
+            res[curr++] = getchar();
+            continue;
+        }
+
+        if (ch == START_SYMBOL) {
+            return curr;
+        }
+    }
+
+    return curr;
+}
+
+int handleStringLiteral(char* res, int curr) {
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        res[curr++] = ch;
+        if (ch == '\\') {
+            res[curr++] = getchar();
+            continue;
+        }
+
+        if (ch == START_STRING) {
+            return curr;
+        }
+    }
+
+    return curr;
+}
