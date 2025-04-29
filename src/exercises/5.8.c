706fd2f9982527cffaa929dadcfed88b54a312b1
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-04-29 22:31:33 +0300

    5.8

--- src/exercises/5.8.c
+++ src/exercises/5.8.c
@@ -22,11 +22,30 @@ int day_of_year(unsigned int year, unsigned int month, unsigned int day) {
   return day;
 }
 
+void month_day(int year, int yearday, int *pmonth, int *pday) {
+  const int leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+  int i = 1;
+  while (yearday > 0) {
+    yearday -= daytab[leap][i++];
+  }
+  yearday += daytab[leap][i - 1];
+
+  *pday = yearday;
+  *pmonth = i;
+}
+
 int main() {
   assertEquals("Check 29.04.25", 119, day_of_year(2025, 4, 29));
   assertEquals("Check 29.04.24", 120, day_of_year(2024, 4, 29));
   assertEquals("Check 32.04.25", -1, day_of_year(2025, 4, 32));
   assertEquals("Check 29.13.25", -1, day_of_year(2025, 13, 32));
+
+  int mday;
+  int month;
+  month_day(2025, 119, &month, &mday);
+  assertEquals("Check 119 2025 month day", 29, mday);
+  assertEquals("Check 119 2025 month", 5, month);
 
   return 0;
 }
