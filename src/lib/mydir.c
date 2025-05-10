df975c0e42350c08788cb6c1a937e333df235f98
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-05-10 15:28:21 +0300

    added fsize

--- src/lib/mydir.c
+++ src/lib/mydir.c
@@ -1,8 +1,10 @@
 #include "mydir.h"
 #include "unistd.h"
 #include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <string.h>
 #include <sys/syscall.h>
 
 typedef struct _derent_buf {
@@ -12,18 +14,28 @@ typedef struct _derent_buf {
   int offset;
 } _dirent_buf;
 
+typedef struct _dir_name {
+  char *n;
+  size_t s;
+} _dir_name;
+
 struct MY_DIR {
   int fd;
+  _dir_name n;
   _dirent_buf *dirent;
 };
+
+// 1, 0, -1 if error
+char _is_dir(const char *);
+char _is_stat_of_dir(const Stat *);
 
 MY_DIR *my_opendir(const char *path) {
   if (path == NULL) {
     return NULL;
   }
 
-  struct stat stat;
-  if ((syscall(SYS_stat, path, &stat) < 0) || (stat.st_mode & S_IFDIR) == 0) {
+  Stat stat;
+  if ((syscall(SYS_stat64, path, &stat) < 0) || !_is_stat_of_dir(&stat)) {
     return NULL;
   }
 
@@ -37,6 +49,10 @@ MY_DIR *my_opendir(const char *path) {
     return NULL;
   }
   dir->fd = fd;
+  dir->n.s = strlen(path);
+  dir->n.n = malloc(dir->n.s + 1);
+  strcpy(dir->n.n, path);
+  dir->n.n[dir->n.s] = '\0';
 
   return dir;
 }
@@ -48,27 +64,69 @@ Dirent *my_readdir(MY_DIR *dir) {
 
   if (dir->dirent == NULL) {
     dir->dirent = malloc(sizeof(_dirent_buf));
-  }
-
-  if (dir->dirent->bytes <= 0) {
-    long esleep = 0;
+    long _ = 0;
     dir->dirent->bytes = syscall(SYS_getdirentries64, dir->fd, dir->dirent->buf,
-                                 sizeof(dir->dirent->buf), &esleep);
+                                 sizeof(dir->dirent->buf), &_);
+    dir->dirent->offset = 0;
   }
 
   _dirent_buf *dirent = dir->dirent;
   if (dirent->bytes <= 0) {
+    free(dirent);
+    dir->dirent = NULL;
     return NULL;
   }
 
-  if ((dirent->d = *((Dirent *)(dirent->buf + dirent->offset))).d_reclen <= 1) {
+  if ((dirent->d = *((Dirent *)(dirent->buf + dirent->offset))).d_reclen == 0) {
+    free(dir->dirent); // if end go to start
     dir->dirent = NULL;
-    free(dirent); // if end go to start
     return NULL;
   }
   dirent->offset += dirent->d.d_reclen;
 
   return &dirent->d;
+}
+
+size_t my_fsize(const char *path) {
+  if (path == NULL) {
+    return 0;
+  }
+
+  Stat stat;
+  if (syscall(SYS_stat64, path, &stat) < 0) {
+    return 0;
+  }
+
+  if (!_is_stat_of_dir(&stat)) {
+    return stat.st_size;
+  }
+
+  MY_DIR *dir = my_opendir(path);
+  if (dir == NULL) {
+    return 0;
+  }
+
+  const int cur_path_len = strlen(path);
+
+  size_t size = 0;
+  Dirent *dirent;
+  while ((dirent = my_readdir(dir)) != NULL) {
+    if (strcmp(".", dirent->d_name) == 0 || strcmp("..", dirent->d_name) == 0) {
+      continue;
+    }
+
+    char name[cur_path_len + dirent->d_namlen + 2]; // 2 - for / an '\0';
+    strcpy(name, path);
+    name[cur_path_len] = '/';
+    strcat(name, dirent->d_name);
+    name[cur_path_len + dirent->d_namlen + 1] = '\0';
+
+    size += my_fsize(name);
+  }
+
+  my_closedir(dir);
+
+  return size;
 }
 
 void my_closedir(MY_DIR *dir) {
@@ -80,5 +138,25 @@ void my_closedir(MY_DIR *dir) {
   if (dir->dirent != NULL) {
     free(dir->dirent);
   }
+  free(dir->n.n);
   free(dir);
+}
+
+char _is_dir(const char *path) {
+  Stat stat;
+  if (syscall(SYS_stat64, path, &stat) < 0) {
+    return -1;
+  }
+
+  return _is_stat_of_dir(&stat);
+}
+
+char _is_stat_of_dir(const Stat *stat) {
+  return (stat->st_mode & S_IFDIR) != 0;
+}
+
+int main() {
+  printf("%u", my_fsize("src"));
+
+  return 0;
 }
--- src/lib/mydir.h
+++ src/lib/mydir.h
@@ -1,13 +1,18 @@
 #ifndef MYDIR_H_
 #define MYDIR_H_
 
+#include <stddef.h>
 #include <sys/dirent.h>
+#include <sys/stat.h>
+
 typedef struct dirent Dirent;
+typedef struct stat Stat;
 typedef struct MY_DIR MY_DIR;
 
 MY_DIR *my_opendir(const char *);
 // returns dir entity info next on each call
 Dirent *my_readdir(MY_DIR *);
+size_t my_fsize(const char *);
 void my_closedir(MY_DIR *);
 
 #endif
